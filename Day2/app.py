49a2ff3349f6ff7c17f5c049f6ff5d5a71e451ca
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2022-10-22 16:42:20 +0100

    day2

---
+++ Day2/app.py
@@ -0,0 +1,121 @@
+# OPERATOR IN PYTHON
+## Arithmetic Oper..
+
+# + add
+# - sub
+# / div
+# * mul
+# % mod
+# // floor div
+# ** exp
+
+# print(2 ** 4)
+
+
+
+## Comperison OP
+# > gt
+# < lt
+# >= gte
+# <= lte
+# == eq
+# != neq
+
+# print(2 == 2)
+
+## Logical OP
+# or
+# and
+# not
+
+# print((2 == 2) or ((3 != 3) and (not 2 != 2)))
+
+# A | B | A or B | A and  B
+# 0 | 0 | 0      | 0
+# 0 | 1 | 1      | 0
+# 1 | 0 | 1      | 0
+# 1 | 1 | 1      | 1
+
+## BIDMAS
+
+## Membership OP
+# in
+
+# print("o" in "Haseeb")
+
+## Bitwise
+# >> rigth shift
+# << left shift
+# & and
+# | or
+# ! not
+
+## Assignment OP
+# =
+
+#Type casting
+
+## Variable
+# x = int(input("Enter a number: "))
+# y = int(input("Enter another number: "))
+# add = x + y
+# print(add)
+
+x = True
+
+# print(dir("Haseeb"))
+# print("HASEEB".upper())
+# print("HASEEB".lower())
+# print("haseeb and me".title())
+# print("haseeb and me".capitalize())
+# print("haseeb and me".startswith("o"))
+
+# friends = ["Scott", "Jason", 23, True, 7.5] # list
+
+# friend = "Scott"
+
+# print(friends[1][4])
+
+
+# RULES IN NAMING A VARIABLE
+#1. A variable can only contain letters[a-z,A-Z], digits[0-9]
+# and an underscore symbol _
+
+#2. A variable must not start with digit
+
+#3. you can't use nay special sybol except an underscore
+
+#4. Whenever you're naming your variable, give it a meaniful name..
+
+# name = "Haseeb"
+# age = 16
+
+#5. Python uses a snake_casing naming confension
+
+first_name_is_something = 23 # snake casing
+FirstNameIsSomething = "James" # Pascal casing
+firstNameIsSomething = "James" # Camel casing
+
+# print()
+
+
+
+
+
+
+# print(name + " is " + str(age))
+
+# Example --> name, first_name, this_is_my_name
+# NAME = "PraiseGod"
+
+# print(NAME)
+
+
+
+
+
+
+
+
+
+
