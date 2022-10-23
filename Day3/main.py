5ab34dc21a35e2e5ccae3c68572b7f9cdc158f69
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2022-10-23 17:03:24 +0100

    third day

---
+++ Day3/main.py
@@ -0,0 +1,144 @@
+# LIST
+
+# names = ["Haseeb", "Scott", "Jason", "Haseeb"]
+# another = [1, 3, 4, 6, 8, 9]
+
+# names.extend(another)
+
+# print(names.count("Haseeb"))
+# print(len(names))
+# print(len("Haseeb"))
+# print(dir(names))
+
+
+# names.append("Haseeb") ## add to the end of the list
+# names.append("Scott")
+# names.clear() # clears all the data from the list
+# another_names = names.copy() #copy the a list
+# another_names.append("PraiseGod")
+
+# # print(dir(names))
+# print("Names: ", names)
+# print("Another names: ", another_names)
+
+
+## STRING FORMATTING
+# name = "Haseeb"
+# age = 16
+# greet = "Hello {}, you are {}, how are you doing?"
+
+# print(greet.format(name, age))
+#
+
+## SIMPLE CALC
+
+# name = input("Enter your name: ")
+# num1 = int(input("Enter 1st number: "))
+# num2 = int(input("Enter 2nd number: "))
+
+# add = num1 + num2
+# result = f"Hello, {name}.. The result for {num1} + {num2} = {add}"
+# print(result)
+
+## Control Structure
+# if 1 != 1:
+#     print("Hello")
+
+## SIMPLE CALC
+
+# name = input("Enter your name: ")
+# option = input("What calculation you wanna perform? (+, -, x, ÷, %) ")
+
+# num1 = int(input("Enter 1st number: "))
+# num2 = int(input("Enter 2nd number: "))
+
+# if option == "+":
+#     result = num1 + num2
+# elif option == "-":
+#     result = num1 - num2
+# elif option == "x":
+#     result = num1 * num2
+# elif option == "÷":
+#     result = num1 / num2
+# elif option == "%":
+#     result = num1 % num2
+# else:
+#     result = "Invalid"
+
+
+
+# message = f"Hello, {name}.. The result for {num1} {option} {num2} = {result}"
+# print(message)
+
+
+
+## LOOPING
+# while
+# correct_num = 1
+# while True:
+#     number = int(input("Guess a number btw 1 and 6: "))
+#     if number == correct_num:
+#         print("Correct")
+#         break
+#     else:
+#         print("Incorrect")
+#         print("Good Bye!")
+
+    # name = input("Enter your name: ")
+
+
+#Multiplication Table Six
+
+
+# count = 1
+# while count <= 12:
+#     output = f"6 x {count} = {6 * count}"
+#     print(output)
+#     count += 1
+#     count = count + 1 --> count += 1
+#     count = count - 1 --> count -= 1
+#     count = count * 1 --> count *= 1
+#     count = count / 1 --> count /= 1
+    
+
+# print("Haseeb"[1])
+# print([1, 2, 3, 4][2])
+
+# name = "Haseeb"
+
+# # for
+# friends = ["Scott", "PG", "Jason"]
+
+# for friend in friends:
+#     print(f"Hello, {friend}, I'm having a birthday party tonight!!")
+
+# i = "H"
+# i = "a"
+
+
+## TUPLE
+# names1 = ["Scott"]
+# names2 = ("Scott", "Jason", "PG") # Iterable
+# # print(names2[1])
+
+# for name in names2:
+#     print(name)
+
+# print(type(names2))
+
+# print("Hello\n") #new line
+# print("\tHello") #horizontal tab 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
