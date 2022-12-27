8b236db3b19faf1f037e55ee8a5f7a21ed710664
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2022-12-27 13:59:33 +0100

    Day6

---
+++ Day5/main.py
@@ -0,0 +1,9 @@
+def add(x, y):
+	return x + y
+
+
+
+operation = {"add": add}
+
+
+print(operation["add"](3, 5))
---
+++ Day6/main.py
@@ -0,0 +1,197 @@
+a = "Something"
+
+# print(type(a))
+# print(help(a))
+# print(dir(a))
+
+# b = 5j # complex type
+
+# print(type(b))
+
+# s = set()
+# s = set() # set
+
+# d = {"name": "Haseeb", "age": 16} # dictionary
+
+# print(dir(s))
+# print(type(s))
+# print(type(d))
+
+
+# n = None
+
+# print(type(n))
+
+# Arithmetic OP
+# +, -, *, /, %, //, **
+
+
+# Comparism
+# >, <, >=, <=, ==, !=
+
+# Logical OPs
+# and, or, not
+# not 5 == 6
+
+# Membership OP\
+# in
+
+# exp = "a" not in "Haseeb"
+
+# print(exp)
+
+# True, str, float, int, bool 
+
+# c = str is not bool
+# # c = 12 == 12
+
+# print(c)
+# age = 18
+
+# name = "Haseeb" if age >= 16 else "PraiseGod" # Ternary Operators
+
+# print(name)
+# = Assignment OP
+
+# a = "Haseeb"
+# b = [3, 6, 2, 7, 9]
+# c = (1,)
+
+# print(type(c))
+
+# print(len(a))
+# print(len(b))
+
+# print(a[3])
+# print(a[-1])
+
+# print(f"My name is {a}!")
+# print("My name is {}".format(a))
+
+
+# name = input("Enter your name: ")
+
+# print(len(name))
+
+# print(f"Your name is {name}")
+
+
+# print("my name \nHaseeb")
+# print("my name \tHaseeb")
+
+# a = set([3, 2, 5])
+# b = bool(a)
+
+# print(b)
+
+# a = "123"
+
+# b = int(a)
+
+# print(type(b))
+
+
+items = ["Goodies", "Cookies", "Cake", "Bread"]
+
+# items.append("Biscuit")
+# items.insert(1, "Chocolate")
+
+# unwanted = items.pop(1)
+
+# del items # I do not advie you use it...
+
+# items.remove("Cake")
+
+# print(unwanted)
+
+# print(items.index("Cake"))
+
+# print(items)
+
+# print(items[1:3])
+
+# name = "Haseeb"
+
+# print(name[0:6:3])
+
+# name = "PG"
+
+# if name == "Haseeb":
+#     print(f"My name is {name}")
+# elif name == "PG":
+#     print(f"My name is {name}")
+# else:
+#     print("I do not know you!")
+
+
+
+# While loop
+
+# counter = 1
+
+# while counter <= 10:
+#     if counter == 6:
+#         continue
+#     print("Run", counter)
+#     counter += 1
+
+
+# import random
+
+# print(random.randint(1, 6))
+
+
+# import tkinter
+
+# def do_somthing():
+#     print("My name is Haseeb")
+
+
+# window = tkinter.Tk()
+# window.geometry("400x400")
+
+# label = tkinter.Label(window, text="Haseeb")
+# label.pack()
+
+# button = tkinter.Button(window, text="Click me", command=do_somthing)
+# button.pack()
+
+# window.mainloop()
+
+
+import turtle
+
+
+pet = turtle.Pen("turtle")
+
+
+for i in range(200):
+    pet.forward(i)
+    pet.right(56)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
