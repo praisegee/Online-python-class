b34c8c47b7b11cc221e432dd2dcfa7624784945e
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2022-12-29 13:32:05 +0100

    Day8

---
+++ Day8/main.py
@@ -0,0 +1,65 @@
+import json
+
+# print(dir(json))
+
+#dump, dumps, load, loads
+
+# name = [1, 'name', True, None, (1, 2), 23.67]
+
+# json_format = json.dumps(name)
+
+# print("Format: ", json_format)
+# print("Type: ", type(json_format))
+
+
+# var = "true"
+
+# jf = json.loads(var)
+
+# print("Format: ", jf)
+# print("Type: ", type(jf))
+
+# file_obj = open('names.txt')
+# name = "Haseeb"
+
+# print(name.isupper())
+
+# print(dir(name))
+
+# print(file_obj)
+
+
+
+
+class Car:
+    
+    def __init__(self, size, color):
+        self.size = size
+        self.color = color
+        print("Created")
+
+    def move():
+        print("Moving....")
+
+    def horn():
+        print("Pinnnn....")
+
+
+car1 = Car(23, "red")
+car2 = Car(12, "blue")
+
+a = "Haseeb" # str
+a.capitalize()
+
+print(car1.size)
+print(car2.size)
+
+
+
+
+
+
+
+
+
+
